8f313286d336701a097b1d8c7963ebe5f5b9d07a
RichardBzar <richardbtzar@gmail.com>
2023-10-30 18:20:56 -1000

    list contacts page mockup finished in 14 minutes

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
-  <Card>
-    <Card.Header></Card.Header>
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Body>
+        <Card.Text>{contact.description}</Card.Text>
+      </Card.Body>
+    </Card.Body>
   </Card>
 );
 
