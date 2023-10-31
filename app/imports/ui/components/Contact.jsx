e3c1de0436d96b4828f9d65dfc1eece8289f754c
RichardBzar <richardbtzar@gmail.com>
2023-10-30 20:11:23 -1000

    update

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const Contact = ({ contacts }) => (
+  <Card>
+    <Card.Header>
+      <Image src={contacts.image} width={75} />
+    </Card.Header>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default StuffItem;
